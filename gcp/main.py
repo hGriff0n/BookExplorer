e059129e2811be7a54c79fd6fa8a79b302e9d16b
hGriff0n <ghooper96@gmail.com>
2019-10-13 09:15:04 -0400

    Added Integration with Goodreads API to GCP Project

--- gcp/main.py
+++ gcp/main.py
@@ -1,6 +1,7 @@
 from flask import Flask
 import logging
 
+# TODO: Move the "gcloud" setup to a gcloud.py file (or something like that)
 # Setup the stackdriver logging agent
 import google.cloud.logging
 stackdriver_logging_client = google.cloud.logging.Client()
@@ -29,13 +30,32 @@ try:
 except (ValueError, NotImplementedError) as exc:
     stackdriver_error_client.report_exception()
 
+# Setup the database client
+from google.cloud import firestore
+
+db = firestore.Client()
+
 # Specify the server
 app = Flask(__name__)
+
+# Read configuration from the databasae
+config = db.collection('configuration')
+api_keys = config.document('api_keys').get()
+
+api_keys = api_keys.to_dict()
+GOODREADS_API_KEY = api_keys['GOODREADS_API_KEY']
+GOODREADS_SECRET_VALUE = api_keys['GOODREADS_API_SECRET']
+logging.info("Goodreads api: {} - {}".format(GOODREADS_API_KEY, GOODREADS_SECRET_VALUE))
+
+# Initialize the goodreads client
+import goodreads_api as goodreads
+gc = goodreads.Client(GOODREADS_API_KEY, GOODREADS_SECRET_VALUE)
 
 @app.route('/')
 def hello():
     # stackdriver_error_client.report("An error has occurred")
-    return 'Hello World2'
+    # return str(gc.book(isbn='978-0241351574'))
+    return '<br>'.join(str(book) for book in gc.get_list(81192485))
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
