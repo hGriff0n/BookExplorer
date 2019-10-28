08ca099f474a617cf49743ca36139a97766755fd
hGriff0n <ghooper96@gmail.com>
2019-10-27 21:47:18 -0400

    I have no idea why TF isn't Working on GCP

--- gcp/server/main.py
+++ gcp/server/main.py
@@ -1,55 +1,62 @@
 from flask import Flask
 import gcloud
-import logging
-
-# Setup the database client
+import goodreads_api as goodreads
 from google.cloud import firestore
-db = firestore.Client()
-
-# # Install the tensorflow module
-# # TODO: Apparently tensorflow requires the "flexible" environment because it loads native code? It doesn't say anything until the 500 error?
+import logging
 # import tensorflow as tf
 # import tensorflow_hub as tf_hub
-# embed_module = tf_hub.Module("https://tfhub.dev/google/universal-sentence-encoder/2")
+
+# Cause "noqa" is apparently disabled
+assert gcloud
+assert logging
+
+# Setup the database client
+db = firestore.Client()
 
 # Specify the server
 app = Flask(__name__)
 
-# Read configuration from the databasae
-config = db.collection('configuration')
+# Install the tensorflow module
+# embed_module = tf_hub.Module("https://tfhub.dev/google/universal-sentence-encoder/2")
+# placeholder = tf.placeholder(dtype=tf.string)
+# embed = embed_module(placeholder)
+# session = tf.Session()
+# session.run([tf.global_variables_initializer(), tf.tables_initializer()])
+
+# Read configuration from the database
 # TODO: Is there a better way of accesing this?
+config = db.collection('configuration')
 api_keys = config.document('api_keys').get().to_dict()
 
 GOODREADS_API_KEY = api_keys['goodreads']['API_KEY']
 GOODREADS_SECRET_VALUE = api_keys['goodreads']['SECRET']
 
 # Initialize the goodreads client
-import goodreads_api as goodreads
 gc = goodreads.Client(GOODREADS_API_KEY, GOODREADS_SECRET_VALUE)
 
 # Get the selection set that we are choosing from
 # TODO: Allow for per-user lists
-# selections = [gc.book(isbn=isbn) for isbn in config.document('choices').get().to_dict()['isbn']]
-# For developing the algorithm use the below line, for "production" use the above line
-selections = [gc.book(isbn=isbn) for isbn in config.document('tests').get().to_dict()['items']]
-
-# Prepare the tensorflow embedding
-# placeholder = tf.placeholder(dtype=tf.string)
-# embed = embed_module(placeholder)
-
-# def compute_embedding(book):
-#     session = tf.Session()
-#     session.run([tf.global_variables_initializer(), tf.tables_initializer()])
-#     return session.run(embed, feed_dict={placeholder: book.description})
+# selections = config.document('choices').get().to_dict()['isbn']
+# For developing the algorithm use the below line, for "production" - above
+selections = config.document('tests').get().to_dict()['items']
+selections = [gc.book(isbn=isbn) for isbn in selections]
 
 
+# TODO: Move this development to a local script (don't have to worry about timeout)
+# Doesn't work on:
+#  AppEngine Standard: TF is a native library, not allowed on standard instances
+#  
+# TODO: Try producing an embedding from the url (might have to custom timeout)
+# TODO: Try loading from gcs to improve speed (might have to custom timeout)
 @app.route('/')
 def hello():
+    # return str(session.run(tf.constant('Hello World')))
+    # return str(session.run(embed, feed_dict={placeholder: "Hello, Tensorflow"}))
     # return '<br>'.join(compute_embedding(gc.book(isbn='978-0241351574')))
     # gcloud.error_client.report("An error has occurred")
-    # return str(gc.book(isbn='978-0241351574'))
+    return str(gc.book(isbn='978-0241351574'))
     # return '<br>'.join(str(book) for book in gc.get_list(81192485))
-    return '<br>'.join(str(book) for book in selections)
+    # return '<br>'.join(str(book) for book in selections)
 
 
 @app.route("/readiness_check")
